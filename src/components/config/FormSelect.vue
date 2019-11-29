2347364721f75c4b43463a2b282ed0b05c1cbecc
hirakumar <hirakumar@gmail.com>
2019-11-29 16:18:04 +0545

    just minor start for selectbox

--- src/components/config/FormSelect.vue
+++ src/components/config/FormSelect.vue
@@ -1,5 +1,25 @@
 <template>
-<div v-html="data.text" class="divEle">asdfasdf</div>
+<b-card   title="Form Group">
+
+  <b-button variant="link" size="md" class="trash" @click="remove">
+      <font-awesome-icon :icon="['fas','trash-alt']" /> 
+  </b-button>
+  <b-list-group flush>
+      <b-list-group-item>
+          <!-- Label -->
+          <b-form-group                        
+            label="Fill Data from : "
+            label-for="label"
+            label-cols="6"
+            class="mb-0"
+            >
+            <b-dropdown right split text="Split Menu">
+              <b-dropdown-item>Add Custom Data</b-dropdown-item>
+          </b-dropdown>
+          </b-form-group>
+      </b-list-group-item>
+  </b-list-group>
+</b-card>
 </template>
 <script>
 
